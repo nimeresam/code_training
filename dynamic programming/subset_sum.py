d0bf2493e508463ead75c7c7ae79e15856a1884a
nimeresam <nimeresam95@gmail.com>
2020-06-23 09:54:03 +0300

    add subset_sum algo

---
+++ dynamic programming/subset_sum.py
@@ -0,0 +1,42 @@
+def isSubsetSum(set, n, sum):
+    # Base Case
+    if sum == 0:
+        return True
+    if n == 0 and sum != 0:
+        return False
+
+    # if the last element is greater than sum
+    # just ignore it
+    if set[n-1] > sum:
+        return isSubsetSum(set, n-1, sum)
+
+    return isSubsetSum(set, n-1, sum) or isSubsetSum(set, n-1, sum-set[n-1])
+
+def isSubsetSum_dyn(set, n, sum):
+    # create 2D array of sum + 1 * n + 1
+    # fill first column with True
+    # fill first row with False
+    # default value is False
+    subset = [[col == 0 for col in range(sum + 1)] for row in range(n+1)]
+
+    # since first col is True
+    # and first row is False
+    # will start from second row and second column
+    for row in range(1, n+1):
+        # find element that we should compare with
+        element = set[row-1]
+        for col in range(1, sum+1):
+            if col < element:
+                subset[row][col] = subset[row-1][col]
+            else:
+                subset[row][col] = subset[row-1][col] or subset[row-1][col-element]
+
+# Driver program to test above function 
+if __name__=='__main__': 
+    set = [3, 34, 4, 12, 5, 2] 
+    sum = 9
+    n = len(set) 
+    if (isSubsetSum(set, n, sum) == True): 
+        print("Found a subset with given sum") 
+    else: 
+        print("No subset with given sum") 
