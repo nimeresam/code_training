a7b466b0d259f0e9cef2f2f602b4c87a2b75941c
nimeresam <nimeresam95@gmail.com>
2023-11-27 08:00:27 +0300

    add cut rod memoization

--- dynamic programming/cut_rod.py
+++ dynamic programming/cut_rod.py
@@ -7,8 +7,23 @@ def cutRod(price, n):
         max_val = max(max_val, price[index] + cutRod(price, n - index - 1))
     return max_val
 
+def cutRodMemo(price, n, memo):
+    if n <= 0:
+        return 0
+    
+    max_val = 0
+    for index in range(n):
+        required = n - index - 1 
+        prev = memo.get(required, None)
+        if not prev:
+            prev = cutRodMem(price, required, memo)
+            memo[required] = prev
+        max_val = max(max_val, price[index] + prev)
+    return max_val
+
 
 # Driver code 
-arr = [1, 8, 5] 
+arr = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30] 
 size = len(arr) 
-print("Maximum Obtainable Value is", cutRod(arr, size)) 
+best_val = cutRodMem(arr, 10, {})
+print("Maximum Obtainable Value is", best_val) 
