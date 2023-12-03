caee03de6e87825029ae3d13e2adbd74cbdbfc99
nimeresam <nimeresam95@gmail.com>
2023-12-03 08:38:28 +0300

    add initial version of mm

---
+++ dynamic programming/matrix_multiplication.py
@@ -0,0 +1,23 @@
+def matrix_multiplication(first, second):
+    def getDimensions(arr):
+        rows = len(arr)
+        cols = len(arr[0])
+        return [rows, cols]
+    [fRs, fCs] = getDimensions(first)
+    [sRs, sCs] = getDimensions(second)
+    if fCs != sRs:
+        raise "Incompaticble"
+
+    arr = [[0 for _ in range(sCs)] for _ in range(fRs)]
+    for ri in range(fRs):
+        for ci in range(sCs):
+            for index in range(fCs):
+                arr[ri][ci] += first[ri][index] * second[index][ci]
+    return arr
+
+# TODO: add the DP method
+result = matrix_multiplication(
+    [[1, 2, 3], [4, 5, 6]],
+    [[1, 4], [2, 5], [3, 6]]
+)
+print(result)
